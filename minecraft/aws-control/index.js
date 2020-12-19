2850ef5a40dac5d95a1aa13afadfdd2a10439e09
Hwanhee Chae <hh@hwan.me>
2020-12-19 17:37:36 +0900

    Add aws-control

---
+++ minecraft/aws-control/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const AWS = require("aws-sdk");
+
+// ~/.aws/credentials
+// [default]
+// aws_access_key_id = <YOUR_ACCESS_KEY_ID>
+// aws_secret_access_key = <YOUR_SECRET_ACCESS_KEY>
+
+AWS.config.update({region: "ap-northeast-2"});
+
+const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+const instance = {
+  InstanceIds: [
+    process.env.INSTANCE_ID
+  ]
+};
+
+const firstArg = process.argv[2];
+
+if (firstArg === "stop") {
+  ec2.stopInstances(instance, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.StoppingInstances[0]);
+    }
+  });
+} else if (firstArg === "start") {
+  ec2.startInstances(instance, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.StartingInstances[0]);
+    }
+  })
+} else {
+  ec2.describeInstances(instance, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const ec2State = data.Reservations[0].Instances[0].State;
+      console.log(ec2State);
+      console.log("Please run script again with an argument: start, stop");
+    }
+  })
+}
