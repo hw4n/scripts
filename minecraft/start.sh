dcb47a54e94d7bd7fb4ccf2800939408e9e1e3cd
Hwanhee Chae <hh@hwan.me>
2020-12-19 16:43:59 +0900

    Add minecraft scripts

---
+++ minecraft/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# minecraft/start.sh only contains command like following
+# java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.16.4-35.1.13.jar nogui
+
+screen -dm -S minecraft bash -c "cd minecraft; ./start.sh"
+
+# also crontab -e and add following
+# @reboot /home/ubuntu/start.sh
---
+++ minecraft/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+screen -p 0 -X stuff "stop^M"
+
+sudo shutdown -h now
