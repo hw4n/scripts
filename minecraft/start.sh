3a6618845418d05ed72eccf8afa522aa18666e73
Hwanhee Chae <hh@hwan.me>
2021-02-21 19:46:53 +0900

    Add -L option on screen this creates screenlog on pwd

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -3,7 +3,7 @@
 # minecraft/start.sh only contains command like following
 # java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.16.4-35.1.13.jar nogui
 
-screen -dm -S minecraft bash -c "cd minecraft; ./start.sh"
+screen -dm -L -S minecraft bash -c "cd minecraft; ./start.sh"
 
 # also crontab -e and add following
 # @reboot /home/ubuntu/start.sh
