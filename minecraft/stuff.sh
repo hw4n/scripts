d552523558beeb832dd85d220ef57736ac803fe7
Hwanhee Chae <hh@hwan.me>
2021-03-01 15:04:54 +0900

    Add more characters to preserve on sed

--- minecraft/stuff.sh
+++ minecraft/stuff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 rm screenlog*
-COMMAND=`sed -e "s/[^ @0-9a-zA-Z']//g" -e 's/ \+/ /' <<< $@`
+COMMAND=`sed -e "s/[^ \{\}\[\]\'\"\:,\.@0-9a-zA-Z']//g" <<< $@`
 screen -p 0 -X stuff "$COMMAND^M"
 
 while [ `cat screenlog.0 | wc -l` -lt 2 ]
